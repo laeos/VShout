f01598b9894082beec2998353b5c1c794879572a
Rex Feany <laeos@laeos.net>
2017-05-07 23:40:05 -0700

    enough to shout to a group with a gui

--- VShout/Form1.Designer.vb
+++ VShout/Form1.Designer.vb
@@ -22,9 +22,114 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.CommandBox = New System.Windows.Forms.TextBox()
+        Me.SendButton = New System.Windows.Forms.Button()
+        Me.LogBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NodeName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(16, 80)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(195, 50)
+        Me.StartButton.TabIndex = 0
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'StopButton
+        '
+        Me.StopButton.Enabled = False
+        Me.StopButton.Location = New System.Drawing.Point(236, 80)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(195, 50)
+        Me.StopButton.TabIndex = 1
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'CommandBox
+        '
+        Me.CommandBox.Enabled = False
+        Me.CommandBox.Location = New System.Drawing.Point(12, 594)
+        Me.CommandBox.Name = "CommandBox"
+        Me.CommandBox.Size = New System.Drawing.Size(687, 26)
+        Me.CommandBox.TabIndex = 3
+        '
+        'SendButton
+        '
+        Me.SendButton.Enabled = False
+        Me.SendButton.Location = New System.Drawing.Point(719, 582)
+        Me.SendButton.Name = "SendButton"
+        Me.SendButton.Size = New System.Drawing.Size(90, 50)
+        Me.SendButton.TabIndex = 4
+        Me.SendButton.Text = "Send!"
+        Me.SendButton.UseVisualStyleBackColor = True
+        '
+        'LogBox
+        '
+        Me.LogBox.Location = New System.Drawing.Point(12, 190)
+        Me.LogBox.Multiline = True
+        Me.LogBox.Name = "LogBox"
+        Me.LogBox.ReadOnly = True
+        Me.LogBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.LogBox.Size = New System.Drawing.Size(687, 398)
+        Me.LogBox.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 167)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Output Log"
+        '
+        'NodeName
+        '
+        Me.NodeName.Location = New System.Drawing.Point(213, 23)
+        Me.NodeName.Name = "NodeName"
+        Me.NodeName.Size = New System.Drawing.Size(380, 26)
+        Me.NodeName.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(195, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Enter Unique Node Name:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(899, 669)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.NodeName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LogBox)
+        Me.Controls.Add(Me.SendButton)
+        Me.Controls.Add(Me.CommandBox)
+        Me.Controls.Add(Me.StopButton)
+        Me.Controls.Add(Me.StartButton)
+        Me.Name = "Form1"
+        Me.Text = "VShout"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents StartButton As Button
+    Friend WithEvents StopButton As Button
+    Friend WithEvents CommandBox As TextBox
+    Friend WithEvents SendButton As Button
+    Friend WithEvents LogBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents NodeName As TextBox
+    Friend WithEvents Label3 As Label
 End Class
--- VShout/Form1.vb
+++ VShout/Form1.vb
@@ -1,3 +1,130 @@
-﻿Public Class Form1
+﻿
 
+Imports NetMQ
+Imports NetMQ.Zyre
+Imports NetMQ.Zyre.ZyreEvents
+
+
+Public Class Form1
+
+    Private _Zyre As Zyre
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        ' dumb unique name
+        NodeName.Text = Guid.NewGuid().ToString()
+
+    End Sub
+
+    ' events do not happen on main/gui thread, so we must push them off on the right thread 
+    Private Sub LogString(ByVal the_log As String)
+        If Me.InvokeRequired Then
+            Dim args() As String = {the_log}
+            Me.Invoke(New Action(Of String)(AddressOf LogString), args)
+            Return
+        End If
+        LogBox.AppendText(the_log)
+    End Sub
+
+    ' new client joined group
+    Private Sub Zyre_JoinEvent(sender As Object, e As ZyreEventJoin)
+        LogString("[" + e.SenderName + "] HAS CONNECTED!" + Environment.NewLine)
+    End Sub
+
+    ' shout to a group received
+    Private Sub Zyre_ShoutEvent(sender As Object, e As ZyreEventShout)
+        Dim ShoutMessage As NetMQMessage
+
+        ShoutMessage = e.Content
+
+        ' unclear how i'm supposed to get just the actual message i put in.. but this works. 
+        LogString("[" + e.SenderName + "] " + ShoutMessage.ToString() + Environment.NewLine)
+    End Sub
+
+    ' saw someone new 
+    Private Sub Zyre_EnterEvent(sender As Object, e As ZyreEventEnter)
+        LogString("detected new node [" + e.SenderName + "]" + Environment.NewLine)
+    End Sub
+
+    ' who knows
+    Private Sub Zyre_StopEvent(sender As Object, e As ZyreEventStop)
+        LogString("RECEIVED: " + e.ToString() + Environment.NewLine)
+    End Sub
+
+    ' remote client left??
+    Private Sub Zyre_ExitEvent(sender As Object, e As ZyreEventExit)
+        LogString("RECEIVED: " + e.ToString() + Environment.NewLine)
+    End Sub
+
+    ' heartbeat failure i think: lost touch with someone
+    Private Sub Zyre_EvasiveEvent(sender As Object, e As ZyreEventEvasive)
+        LogString("RECEIVED: " + e.ToString() + Environment.NewLine)
+    End Sub
+
+    ' someone leaves the group
+    Private Sub Zyre_LeaveEvent(sender As Object, e As ZyreEventLeave)
+        LogString("RECEIVED: " + e.ToString() + Environment.NewLine)
+    End Sub
+
+    ' direct message from a client
+    Private Sub Zyre_WhisperEvent(sender As Object, e As ZyreEventWhisper)
+        LogString("RECEIVED: " + e.ToString() + Environment.NewLine)
+    End Sub
+
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        _Zyre = New Zyre(NodeName.Text)
+
+        NodeName.Enabled = False
+        StartButton.Enabled = False
+        StopButton.Enabled = True
+        SendButton.Enabled = True
+        CommandBox.Enabled = True
+
+        LogBox.AppendText("starting up" + Environment.NewLine)
+
+        AddHandler _Zyre.JoinEvent, AddressOf Zyre_JoinEvent
+        AddHandler _Zyre.ShoutEvent, AddressOf Zyre_ShoutEvent
+        AddHandler _Zyre.EnterEvent, AddressOf Zyre_EnterEvent
+        AddHandler _Zyre.StopEvent, AddressOf Zyre_StopEvent
+        AddHandler _Zyre.ExitEvent, AddressOf Zyre_ExitEvent
+        AddHandler _Zyre.EvasiveEvent, AddressOf Zyre_EvasiveEvent
+        AddHandler _Zyre.LeaveEvent, AddressOf Zyre_LeaveEvent
+        AddHandler _Zyre.WhisperEvent, AddressOf Zyre_WhisperEvent
+
+        ' set public name
+        _Zyre.SetName(NodeName.Text)
+        _Zyre.Start() ' starts broadcast discovery
+        _Zyre.Join("HappyGroup") ' join a group: fixed name for now
+
+    End Sub
+
+    Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        NodeName.Enabled = True
+        StartButton.Enabled = True
+        StopButton.Enabled = False
+        SendButton.Enabled = False
+        CommandBox.Enabled = False
+
+        _Zyre.Stop()
+        _Zyre = Nothing
+    End Sub
+
+    Private Sub SendButton_Click(sender As Object, e As EventArgs) Handles SendButton.Click
+        Dim ShoutMessage As NetMQMessage
+
+        ShoutMessage = New NetMQMessage()
+
+        ShoutMessage.Append(CommandBox.Text)
+        _Zyre.Shout("HappyGroup", ShoutMessage) ' send message to group, in this case HappyGroup
+
+        LogBox.AppendText("SHOUT: " + CommandBox.Text + Environment.NewLine)
+        CommandBox.Text = ""
+
+    End Sub
 End Class
